ae303f101a4f48e7486936c6cc3fbac395af68c2
Torito9609 <toroandres85@gmail.com>
2026-02-19 14:52:52 -0500

    test newList_ZeroEmpty passed

--- test/linkedlist/SinglyLinkedListTest.java
+++ test/linkedlist/SinglyLinkedListTest.java
@@ -1,4 +1,31 @@
 package linkedlist;
 
-public class SinglyLinkedListTest {
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SinglyLinkedListTest {
+    LinkedList<Integer> list;
+
+    @BeforeEach
+    void createEmptyList(){
+        list = new SinglyLinkedList<>();
+    }
+
+    @Test
+    void newList_isEmptyAndZero(){
+        assertEquals(0,list.size());
+        assertTrue(list.isEmpty());
+        assertFalse(list.contains(0));
+        assertEquals(-1, list.indexOf(0));
+        assertFalse(list.remove(0));
+        list.clear();
+        assertEquals(0,list.size());
+        assertTrue(list.isEmpty());
+        assertFalse(list.contains(0));
+        assertEquals(-1, list.indexOf(0));
+        assertFalse(list.remove(0));
+    }
 }
